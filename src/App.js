ed4d53643f05972355e5cadf31217b876a2395c2
Veera Sankar <sankarsivam6991@gmail.com>
2025-04-28 17:21:26 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-green-500">
+      <h1 className="text-3xl font-bold underline text-center text-white">
+        hello world
+      </h1>
+    </div>
+  );
+}
+
+export default App;
